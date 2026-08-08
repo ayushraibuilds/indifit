5856ef22a20e0632a25d25fb93a21c10fd5c2f61
Ayush Rai <ayushrai420559@gmail.com>
2026-08-08 16:53:24 +0530

    test(ux): sort Wave 3 imports

--- test/ux_w03_today_redesign_test.dart
+++ test/ux_w03_today_redesign_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:indifit/core/nutrients.dart';
